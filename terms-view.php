81e3ee212e757a70180bed6efc69d0d3f757fc50
Manoj Pal <manojk.pal@gmail.com>
2022-08-02 15:47:06 +0530

    update

---
+++ terms-view.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+include("include/config.php");
+include("include/functions.php"); 
+validate_admin();
+    	      
+$sql=$obj->query("select terms from $tbl_setting where id=1",$debug=-1);
+$result=$obj->fetchNextObject($sql);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include('head.php') ?>
+	<link href="assets/plugins/dropzone/dropzone.css" rel="stylesheet" media="screen">
+	<link href="assets/plugins/summernote/summernote.css" rel="stylesheet">
+	 <link rel="stylesheet" href="assets/css/select2.min.css">
+</head>
+<body
+	class="page-header-fixed sidemenu-closed-hidelogo page-content-white page-md header-white white-sidebar-color logo-indigo">
+	<div class="page-wrapper">
+		<?php include("topmenu.php"); ?>
+		<div class="page-container">
+			<!-- start sidebar menu -->
+			<div class="sidebar-container">
+				<?php include("sidemenu.php"); ?>
+			</div>
+			<!-- end sidebar menu -->
+			<!-- start page content -->
+			<div class="page-content-wrapper">
+				<div class="page-content">
+					<div class="page-bar">
+						<div class="page-title-breadcrumb">
+							<div class=" pull-left">
+								<div class="page-title">Terms</div>
+							</div>
+							<ol class="breadcrumb page-breadcrumb pull-right">
+								<li><i class="fa fa-home"></i>&nbsp;<a class="parent-item"
+										href="welcome.php">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
+								</li>
+
+							</ol>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="card-box">
+								<div class="card-head">
+									<header>Terms</header>
+									<button id="panel-button"
+										class="mdl-button mdl-js-button mdl-button--icon pull-right"
+										data-upgraded=",MaterialButton">
+										
+									</button>
+								
+								</div>
+								<div class="card-body row">									
+									<p><?php echo $result->terms; ?></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<
+				</div>
+			</div>
+
+		</div>
+		<div class="page-footer">
+			<?php include("footer.php"); ?>
+		</div>
+	</div>
+
+</body>
+	<script src="assets/plugins/dropzone/dropzone.js"></script>
+	<script src="assets/plugins/dropzone/dropzone-call.js"></script>
+	<script src="assets/plugins/summernote/summernote.js"></script>
+	<script src="assets/js/pages/summernote/summernote-data.js"></script>
+<script>
+		$(document).ready(function(){
+			$("#countryfrm").validate();
+		})
+	</script>
+	<style type="text/css">
+		.error{
+			color: red;
+		}
+	</style>
+</html>
